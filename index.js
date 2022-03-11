d1450f993111e4cd16c5f140de102be56b43d509
haerphi <haerphi@gmail.com>
2022-03-11 08:46:19 +0100

    add prettier

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 var express = require("express");
 var app = express();
 
-const { APP_PORT, PORT } = process.env;
+const {APP_PORT, PORT} = process.env;
 const port = APP_PORT || PORT || 8080;
 
 console.log(process.env);
 
 app.use("/static", express.static("public"));
 
-app.get("/timeout", function(req, res) {
-  const delay = 60;
-  console.log("timeout incoming !");
-  setTimeout(() => {
-    res.send(`Timeout of ${delay}s`);
-  }, delay * 1000);
+app.get("/timeout", function (req, res) {
+    const delay = 60;
+    console.log("timeout incoming !");
+    setTimeout(() => {
+        res.send(`Timeout of ${delay}s`);
+    }, delay * 1000);
 });
 
-app.get("/timeout10", function(req, res) {
-  const delay = 10;
-  console.log("timeout incoming !");
-  setTimeout(() => {
-    res.send(`Timeout of ${delay}s`);
-  }, delay * 1000);
+app.get("/timeout10", function (req, res) {
+    const delay = 10;
+    console.log("timeout incoming !");
+    setTimeout(() => {
+        res.send(`Timeout of ${delay}s`);
+    }, delay * 1000);
 });
 
-app.get("/", function(req, res) {
-  res.send("Hello world");
+app.get("/", function (req, res) {
+    res.send("Hello world");
 });
 
-app.listen(port, function() {
-  console.log("http://localhost:8080");
+app.listen(port, function () {
+    console.log("http://localhost:8080");
 });
